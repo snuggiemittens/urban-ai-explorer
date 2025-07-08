db7c09934d03374aca7cb6a47cb35ebc493f8f57
Sean Young <darthvader@Mac.lan>
2025-07-08 16:31:48 -0400

    pushing cors changes

--- backend/src/main/java/com/urban/explorer/controller/ApiController.java
+++ backend/src/main/java/com/urban/explorer/controller/ApiController.java
@@ -12,7 +12,7 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class ApiController {
 
     @Autowired
