7c4ed88c9a52e5fd6a721c89c26c54a7e8ea595f
Sean Young <darthvader@Mac.lan>
2025-07-10 11:35:40 -0400

    cleaning up extra dependencies

--- frontend/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import MatrixConversationalProgramming from './MatrixConversationalProgramming'; // You'll need to save the artifact as this file
-
-function App() {
-  return <MatrixConversationalProgramming />;
-}
-
-export default App;
