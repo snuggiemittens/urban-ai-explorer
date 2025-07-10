1d69b784b4f8081d473b722fa8e8e7923eaf7dfb
Sean Young <darthvader@Mac.lan>
2025-07-10 11:38:36 -0400

    Recover frontend folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import MatrixConversationalProgramming from './MatrixConversationalProgramming'; // You'll need to save the artifact as this file
+
+function App() {
+  return <MatrixConversationalProgramming />;
+}
+
+export default App;
